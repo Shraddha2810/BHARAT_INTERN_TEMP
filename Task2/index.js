46bed998ae8eace53bf85d9c5ff4fd96e1bdc86b
Shraddha2810 <107669369+Shraddha2810@users.noreply.github.com>
2023-11-13 16:52:25 +0530

    Add files via upload

---
+++ Task2/index.js
@@ -0,0 +1,24 @@
+let c = document.getElementById("Celsius");
+let k = document.getElementById("Kelvin");
+let f = document.getElementById("Fahrenheit");
+
+
+ c.addEventListener("input",function(){
+    f.value=parseFloat((c.value)*9/5)+32;
+    k.value=parseFloat(c.value)+273.15;
+ })
+ 
+ k.addEventListener("input",function(){
+    f.value=parseFloat((k.value-273.15)*9/5)+32;
+    c.value=parseFloat(k.value)-273.15;
+ })
+
+ f.addEventListener("input",function(){
+   c.value=parseFloat((f.value-32)*5/9);
+   k.value=parseFloat((f.value-32)*5/9+273.15);
+})
+function agian() {
+c.value = "";
+k.value = "";
+f.value = "";
+}
